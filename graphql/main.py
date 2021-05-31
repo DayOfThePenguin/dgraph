9a3442f5dcb173a7d1ea9ca323bee23ddc72e430
DayOfThePenguin <colin.dablain@gmail.com>
2021-05-31 13:52:51 -0700

    got basic fastapi hello world graphql endpoint working

---
+++ graphql/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import graphene
+from starlette.graphql import GraphQLApp
+
+from schema import Query
+
+app = FastAPI()
+
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query)))
+
+@app.get('/')
+def ping():
+    return {"ping": "pong"}
