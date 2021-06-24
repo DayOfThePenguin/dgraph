706cf6ed7fe488e440d3ca6457024e37ab7a539f
DayOfThePenguin <colin.dablain@gmail.com>
2021-06-23 22:10:07 -0700

    made it most of the way through the tutorial

--- graphql/main.py
+++ graphql/main.py
@@ -1,12 +1,16 @@
+# https://testdriven.io/blog/fastapi-graphql/
+# https://github.com/graphql-python/graphene-pydantic
+# https://dgraph.io/docs/clients/python/
+
 from fastapi import FastAPI
 import graphene
 from starlette.graphql import GraphQLApp
 
-from schema import Query
+from schema import Query, Mutation
 
 app = FastAPI()
 
-app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query)))
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
 
 @app.get('/')
 def ping():
---
+++ graphql/notes.md
@@ -0,0 +1,3 @@
+## Orator Commands
+orator migrate -c db.py
+orator make:model Post -m
